b315573d5c428248eeac03984f808b2f241a81cf
Gaviobm1 <142741214+Gaviobm1@users.noreply.github.com>
2024-02-27 12:46:51 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "./src/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|otf|ttf|eot)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(gif|jpeg|jpg|svg|png|bmp|ico|tiff|webp|avif|apng)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "Document",
+      inject: "body",
+    }),
+  ],
+  watch: true,
+};
